2aa2983d5cd3ae57f613ce11fcd1fdbb44a3608d
AixAria <3x14159265@protonmail.ch>
2026-08-29 01:03:03 -0700

    Create wal.rs

---
+++ rust_engine/src/wal.rs
@@ -0,0 +1,50 @@
+use std::fs::{OpenOptions, File};
+use std::io::{Write, Read, Seek, SeekFrom};
+use std::path::Path;
+
+pub struct WriteAheadLog {
+    file: File,
+}
+
+impl WriteAheadLog {
+    pub fn open<P: AsRef<Path>>(path: P) -> std::io::Result<Self> {
+        let file = OpenOptions::new()
+            .create(true)
+            .read(true)
+            .append(true)
+            .open(path)?;
+        Ok(Self { file })
+    }
+
+    pub fn append_entry(&mut self, view: u64, seq: u64, phase_u8: u8, digest: &[u8; 32]) -> std::io::Result<()> {
+        self.file.write_all(&view.to_be_bytes())?;
+        self.file.write_all(&seq.to_be_bytes())?;
+        self.file.write_all(&[phase_u8])?;
+        self.file.write_all(digest)?;
+        self.file.flush()?;
+        Ok(())
+    }
+
+    pub fn replay_log<F>(&mut self, mut callback: F) -> std::io::Result<()> 
+    where
+        F: FnMut(u64, u64, u8, [u8; 32])
+    {
+        self.file.seek(SeekFrom::Start(0))?;
+        let mut buf = Vec::new();
+        self.file.read_to_end(&mut buf)?;
+
+        let mut cursor = 0;
+        while cursor + 41 <= buf.len() {
+            let view = u64::from_be_bytes(buf[cursor..cursor+8].try_into().unwrap());
+            let seq = u64::from_be_bytes(buf[cursor+8..cursor+16].try_into().unwrap());
+            let phase_u8 = buf[cursor+16];
+            let mut digest = [0u8; 32];
+            digest.copy_from_slice(&buf[cursor+17..cursor+49]);
+
+            callback(view, seq, phase_u8, digest);
+            cursor += 49;
+        }
+        Ok(())
+    }
+}
+
