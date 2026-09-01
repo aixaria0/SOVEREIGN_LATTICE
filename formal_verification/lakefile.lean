79d5b4210696af1f96fdc1f4b0f872db8563484c
AixAria <3x14159265@protonmail.ch>
2026-09-01 21:18:22 +0330

    Update mathlib version to v4.34.0-rc2

--- formal_verification/lakefile.lean
+++ formal_verification/lakefile.lean
@@ -4,7 +4,7 @@ open Lake DSL
 package «sovereign_lattice»
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.34.0-rc2"
 
 @[default_target]
 lean_lib «GodelLobBFT»
