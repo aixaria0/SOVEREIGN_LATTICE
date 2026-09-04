ecef57e5f0f1333fcdf3cd70db60259ad6722a8b
AixAria <3x14159265@protonmail.ch>
2026-09-05 02:51:46 +0330

    Update module visibility in lib.rs
    Reorganize module exports in lib.rs

--- rust_engine/src/lib.rs
+++ rust_engine/src/lib.rs
@@ -1,10 +1,5 @@
+pub mod dkg;
+pub mod pedersen_vss;
 pub mod threshold_bls;
-pub mod wal;
 pub mod pbft;
-pub mod pbft_state;
-pub mod consensus_engine;
-pub mod quorum_tracker;
-pub mod network;
-
-#[cfg(test)]
-pub mod stateful_fuzz;
+pub mod wal;
