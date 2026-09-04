cc171285a02f848304047f46711dec47e196003c
AixAria <3x14159265@protonmail.ch>
2026-09-04 17:54:24 +0330

    Refactor NodeState to use HashMaps
    Refactor NodeState to use HashMaps for prepared and committed states, and update related methods.

--- rust_engine/src/pbft_state.rs
+++ rust_engine/src/pbft_state.rs
@@ -1,33 +1,34 @@
+use std::collections::HashMap;
+
 pub type NodeId = u32;
 pub type Digest = [u8; 32];
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct View(pub u64);
 
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub enum NodeState {
-    Normal,
-    ViewChange,
-    Recovering,
-}
-
-impl Default for NodeState {
-    fn default() -> Self {
-        NodeState::Normal
-    }
+#[derive(Debug, Clone, Default)]
+pub struct NodeState {
+    pub prepared: HashMap<(View, u64), Digest>,
+    pub committed: HashMap<(View, u64), Digest>,
 }
 
 impl NodeState {
     pub fn new() -> Self {
-        NodeState::Normal
+        Self::default()
     }
 
-    pub fn mark_prepared(&mut self, _view: View, _seq: u64, _digest: Digest) -> Result<(), &'static str> {
+    pub fn mark_prepared(&mut self, view: View, seq: u64, digest: Digest) -> Result<(), &'static str> {
+        self.prepared.insert((view, seq), digest);
         Ok(())
     }
 
-    pub fn mark_committed(&mut self, _view: View, _seq: u64, _digest: Digest) -> Result<(), &'static str> {
+    pub fn mark_committed(&mut self, view: View, seq: u64, digest: Digest) -> Result<(), &'static str> {
+        self.committed.insert((view, seq), digest);
         Ok(())
+    }
+
+    pub fn is_committed(&self, view: View, seq: u64, digest: &Digest) -> bool {
+        self.committed.get(&(view, seq)) == Some(digest)
     }
 }
 
@@ -41,7 +42,7 @@ pub struct ViewChangeVote {
 #[derive(Debug, Clone)]
 pub struct NewViewCertificate {
     pub target_view: View,
-    pub view_change_votes: std::collections::HashMap<NodeId, ViewChangeVote>,
+    pub view_change_votes: HashMap<NodeId, ViewChangeVote>,
 }
 
 impl NewViewCertificate {
