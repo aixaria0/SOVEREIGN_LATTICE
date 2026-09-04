e2852a1dd7e65db16ff969c17d413867fb3785f3
AixAria <3x14159265@protonmail.ch>
2026-09-05 00:24:48 +0330

    Add PedersenCommitment struct with methods

---
+++ rust_engine/src/pedersen_vss.rs
@@ -0,0 +1,33 @@
+use bls12_381::{G2Projective, Scalar};
+use rand::rngs::OsRng;
+use ff::Field;
+use crate::threshold_bls::independent_nums_g2_generator;
+
+pub struct PedersenCommitment {
+    pub g: G2Projective,
+    pub h: G2Projective,
+}
+
+impl PedersenCommitment {
+    pub fn new() -> Self {
+        Self {
+            g: G2Projective::generator(),
+            h: independent_nums_g2_generator(),
+        }
+    }
+
+    pub fn commit(&self, secret: &Scalar, blinding_factor: &Scalar) -> G2Projective {
+        (self.g * secret) + (self.h * blinding_factor)
+    }
+
+    pub fn generate_blinding_factor() -> Scalar {
+        Scalar::random(&mut OsRng)
+    }
+}
+
+impl Default for PedersenCommitment {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
