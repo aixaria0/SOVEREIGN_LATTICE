de0fe86dc9ff99faa9a21b7b4f34074f92286186
AixAria <3x14159265@protonmail.ch>
2026-09-04 17:24:44 +0330

    Replace modules in lib.rs with new ones

--- rust_engine/src/lib.rs
+++ rust_engine/src/lib.rs
@@ -1,5 +1,3 @@
-pub mod pbft;
-pub mod stateful_fuzz;
-// If you have these other modules already, keep them here:
-// pub mod threshold_bls;
-// pub mod wal;
+pub mod pbft_state;
+pub mod quorum_tracker;
+pub mod consensus_engine;
